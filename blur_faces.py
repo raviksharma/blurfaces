3cf58290bf755893d7ebea131a1073e82ff18b9a
Ravi Kant Sharma <600723+raviksharma@users.noreply.github.com>
2022-11-27 21:02:37 +0100

    adding cmd line options

--- blur_faces.py
+++ blur_faces.py
@@ -1,56 +1,66 @@
 import os
-import sys
+import click
 import random
 import ffmpeg
 import face_recognition
 import cv2
 from tqdm import trange
 
-in_video_file = sys.argv[1]
-out_video_file = ''.join(random.choice('0123456789ABCDEF') for i in range(6)) + '_blurred_faces_' + in_video_file
 
-video_capture = cv2.VideoCapture(in_video_file)
-in_av_file = ffmpeg.input(in_video_file)
-a1 = in_av_file.audio
+def get_out_file(file_extension):
+    return ''.join(random.choice('0123456789ABCDEF') for i in range(6)) + '_video_only' + file_extension
 
-width = int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
-length = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
 
-fps = video_capture.get(cv2.CAP_PROP_FPS)
+def decode_fourcc(cc):
+    return ''.join([chr((int(cc) >> 8 * i) & 0xFF) for i in range(4)])
 
-print(width, height, length, fps)
 
-video_out = cv2.VideoWriter(
-            out_video_file, cv2.VideoWriter_fourcc(*'mp4v'), fps, (width, height))
+@click.command()
+@click.argument('in_video_file', type=click.Path(exists=True))
+def blurfaces(in_video_file):
+    click.echo(click.format_filename(in_video_file))
 
-face_locations = []
+    filename, file_extension = os.path.splitext(in_video_file)
+    in_av_file = ffmpeg.input(in_video_file)
+    a1 = in_av_file.audio
 
-for i in trange(length+1):
-    # Grab a single frame of video
-    ret, frame = video_capture.read()
-    if not ret:
-        video_out.release()
-        break
+    video_capture = cv2.VideoCapture(in_video_file)
+    width = int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    length = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
+    fps = video_capture.get(cv2.CAP_PROP_FPS)
+    fourcc = video_capture.get(cv2.CAP_PROP_FOURCC)
+    codec = decode_fourcc(fourcc)
+    print(f'{width=} {height=} {length=} {fps=} {codec=}')
 
-    face_locations = face_recognition.face_locations(frame)
-    for (top, right, bottom, left) in face_locations:
+    out_video_file = get_out_file(file_extension)
+    video_out = cv2.VideoWriter(
+        out_video_file, int(fourcc), fps, (width, height))
 
-        # Extract the region of the image that contains the face
-        face_image = frame[top:bottom, left:right]
+    face_locations = []
+    for i in trange(length+1):
+        ret, frame = video_capture.read()
+        if not ret:
+            video_out.release()
+            break
 
-        # Blur the face
-        blurred_face = cv2.GaussianBlur(face_image, (0, 0), 30)
+        face_locations = face_recognition.face_locations(frame)
+        for (top, right, bottom, left) in face_locations:
+            face_image = frame[top:bottom, left:right]
+            blurred_face = cv2.GaussianBlur(face_image, (0, 0), 30)
+            frame[top:bottom, left:right] = blurred_face
+        video_out.write(frame)
 
-        # Put the blurred face region back into the frame image
-        frame[top:bottom, left:right] = blurred_face
+    video_capture.release()
+    cv2.destroyAllWindows()
 
-    video_out.write(frame)
+    stream = ffmpeg.input(out_video_file)
+    stream = ffmpeg.output(stream, a1, 'out' + file_extension)
+    ffmpeg.run(stream)
+    os.remove(out_video_file)
 
-video_capture.release()
-cv2.destroyAllWindows()
+    return 0
 
-stream = ffmpeg.input(out_video_file)
-stream = ffmpeg.output(stream, a1, 'audio_' + out_video_file)
-ffmpeg.run(stream)
-os.remove(out_video_file)
+
+if __name__ == '__main__':
+    blurfaces()
