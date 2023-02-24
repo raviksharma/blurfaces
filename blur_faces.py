be4d0c81051621626fa27e99e7977c3935dd1d54
Ravi Kant Sharma <600723+raviksharma@users.noreply.github.com>
2023-02-24 14:52:09 +0530

    adding mode param

--- blur_faces.py
+++ blur_faces.py
@@ -20,23 +20,25 @@ def has_audio(file_path):
     return False
 
 
+def get_video_properties(video_capture):
+    width = int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    length = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
+    fps = video_capture.get(cv2.CAP_PROP_FPS)
+    fourcc = video_capture.get(cv2.CAP_PROP_FOURCC)
+    codec = decode_fourcc(fourcc)
+    return width, height, length, fps, fourcc, codec
+
+
 @click.command()
+@click.option('--mode', default='all', type=click.Choice(['all', 'one', 'allexcept'], case_sensitive=False))
 @click.option('--model', default='hog', type=click.Choice(['hog', 'cnn'], case_sensitive=False))
-@click.option('--censor_type', default='gaussianblur', type=click.Choice(['gaussianblur', 'facemasking', 'pixelation'], case_sensitive=False))
+@click.option('--censor-type', default='gaussianblur', type=click.Choice(['gaussianblur', 'facemasking', 'pixelation'], case_sensitive=False))
 @click.option('--count', default=1, help='How many times to upsample the image looking for faces. Higher numbers find smaller faces.')
 @click.argument('in_video_file', type=click.Path(exists=True))
-def blurfaces(model, censor_type, count, in_video_file):
+def blurfaces(mode, model, censor_type, count, in_video_file):
     click.echo(click.format_filename(in_video_file))
-    print(f'{model=}')
-    print(f'{censor_type=}')
-    print(f'{count=}')
-
-    ross1 = face_recognition.load_image_file('Ross_Geller.jpg')
-    try:
-        # expecting only 1 face in the image
-        ross1_enc = face_recognition.face_encodings(ross1)[0]
-    except IndexError:
-        print('face not found in image')
+    print(f'{mode=}, {model=}, {censor_type=}, {count=}')
 
     _, file_extension = os.path.splitext(in_video_file)
 
@@ -44,35 +46,40 @@ def blurfaces(model, censor_type, count, in_video_file):
     a1 = in_av_file.audio if has_audio(in_video_file) else None
 
     video_capture = cv2.VideoCapture(in_video_file)
-    width = int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    length = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
-    fps = video_capture.get(cv2.CAP_PROP_FPS)
-    fourcc = video_capture.get(cv2.CAP_PROP_FOURCC)
-    codec = decode_fourcc(fourcc)
-    print(f'{width=} {height=} {length=} {fps=} {codec=}')
+    width, height, length, fps, fourcc, codec = get_video_properties(video_capture)
+    print(f'{width=}, {height=}, {length=}, {fps=}, {codec=}')
 
     with tempfile.NamedTemporaryFile(suffix=file_extension) as out_video_file:
         video_out = cv2.VideoWriter(
             out_video_file.name, int(fourcc), fps, (width, height))
 
         face_locations = []
-        for i in trange(length+1):
-            ret, frame = video_capture.read()
-            if not ret:
-                video_out.release()
-                break
+        if mode == 'one':
+            ross1 = face_recognition.load_image_file('Ross_Geller.jpg')
+            try:
+                # expecting only 1 face in the image
+                ross1_enc = face_recognition.face_encodings(ross1)[0]
+            except IndexError:
+                print('face not found in image')
 
-            face_locations = face_recognition.face_locations(frame, number_of_times_to_upsample=count, model=model)
-            face_image_encodings = face_recognition.face_encodings(frame, face_locations)
-            results = face_recognition.compare_faces(face_image_encodings, ross1_enc)
-            for found, face_location in zip(results, face_locations):
-                if found == True:
-                    top, right, bottom, left  = face_location
-                    face_image = frame[top:bottom, left:right]
-                    blurred_face = np.zeros((bottom-top, right-left, 3))
-                    frame[top:bottom, left:right] = blurred_face
-            video_out.write(frame)
+            for i in trange(length+1):
+                ret, frame = video_capture.read()
+                if not ret:
+                    video_out.release()
+                    break
+
+                face_locations = face_recognition.face_locations(frame, number_of_times_to_upsample=count, model=model)
+                face_image_encodings = face_recognition.face_encodings(frame, face_locations)
+                results = face_recognition.compare_faces(face_image_encodings, ross1_enc)
+                for found, face_location in zip(results, face_locations):
+                    if found:
+                        top, right, bottom, left = face_location
+                        # face_image = frame[top:bottom, left:right]
+                        blurred_face = np.zeros((bottom-top, right-left, 3))
+                        frame[top:bottom, left:right] = blurred_face
+                video_out.write(frame)
+        else:
+            exit('mode not supported.')
 
         video_capture.release()
         cv2.destroyAllWindows()
